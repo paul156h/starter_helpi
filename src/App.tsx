6a3d896793ddd33abfa3b8c3419ebdf2aa50212d
Brandon Cell <bcell@udel.edu>
2024-04-03 16:51:21 -0400

    added name

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>
+        Brandon Cell
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
