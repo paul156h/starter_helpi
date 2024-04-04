d9d1063e3ac7407fd30b1886248b392ef12d78a5
Matthew <mmontal@udel.edu>
2024-04-04 17:03:11 -0400

    Created the button for Basic Career Assessment page and should navigate to new page

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,9 @@ function App() {
   function changeKey(event: React.ChangeEvent<HTMLInputElement>) {
     setKey(event.target.value);
   }
+  function navigateToCareerAssessmentPage() {
+    window.location.href = '/Basic-Career-assessment';
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -49,6 +52,7 @@ function App() {
         <br></br>
         <Button className="Submit-Button" onClick={handleSubmit}>Submit</Button>
       </Form>
+      <Button className="Basic-Career-Assessment-Button" onClick={navigateToCareerAssessmentPage}>Basic Career Asessment Page</Button>
     </div>
   );
 }
