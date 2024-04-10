01c3fc1c56a5f4545827cdb9b67fec426d0e6571
Brandon Cell <bcell@udel.edu>
2024-04-10 19:25:40 -0400

    finished BasicQuestions.tsx component and set up questions for basicPage

--- src/pages/basicPage.tsx
+++ src/pages/basicPage.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+import { BasicQuestions } from "../components/BasicQuestions";
 
 export function BasicPage() {
   return (
+    //The Basic Page will have questions be answered in mulitple choice form
     <>
       <h1>Basic Questionnaire</h1>
+      <hr></hr>
+      <BasicQuestions question="(add question 1 here)" questionNumber="1" answer1="yes" answer2="no" answer3="maybe" answer4="idk bruh"></BasicQuestions>
+      <BasicQuestions question="(add question 2 here)" questionNumber="2" answer1="yes" answer2="no" answer3="maybe" answer4="idk bruh"></BasicQuestions>
+      <BasicQuestions question="(add question 3 here)" questionNumber="3" answer1="yes" answer2="no" answer3="maybe" answer4="idk bruh"></BasicQuestions>
+      <BasicQuestions question="(add question 4 here)" questionNumber="4" answer1="yes" answer2="no" answer3="maybe" answer4="idk bruh"></BasicQuestions>
+      <BasicQuestions question="(add question 5 here)" questionNumber="5" answer1="yes" answer2="no" answer3="maybe" answer4="idk bruh"></BasicQuestions>
+      <BasicQuestions question="(add question 6 here)" questionNumber="6" answer1="yes" answer2="no" answer3="maybe" answer4="idk bruh"></BasicQuestions>
+      <BasicQuestions question="(add question 7 here)" questionNumber="7" answer1="yes" answer2="no" answer3="maybe" answer4="idk bruh"></BasicQuestions>
+      <h4>add more questions if needed</h4>
     </>
+    //this can be stylized later to look better, but for now this is the basic setup
   );
 }
