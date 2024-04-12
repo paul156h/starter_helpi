d17eb3f4db928619d3b012efcf2bdcd600c2a2db
Nazmul <nazmul@udel.edu>
2024-04-12 17:53:41 -0400

    hover animation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,19 +2,25 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import joblogo from "../images/job-Logo.png";
 
 export function Navbar() {
   return (
-    <div className="navLinks">
-      <Link to="/">
-        <Button className="home-btn">Home</Button>
-      </Link>
-      <Link to="/basicPage">
-        <Button className="home-btn">Basic</Button>
-      </Link>
-      <Link to="/detailedPage">
-        <Button className="home-btn">Detailed</Button>
-      </Link>
+    <div className="navBar">
+      <div>
+        <img src={joblogo} alt="" className="logo" />
+      </div>
+      <div className="navLinks">
+        <Link to="/">
+          <Button className="home-btn">Home</Button>
+        </Link>
+        <Link to="/basicPage">
+          <Button className="basic-btn">Basic</Button>
+        </Link>
+        <Link to="/detailedPage">
+          <Button className="detailed-btn">Detailed</Button>
+        </Link>
+      </div>
     </div>
   );
 }
