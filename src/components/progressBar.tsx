b3e76b7a4489a7525c7c431edea887fe40c35332
Brandon Cell <bcell@udel.edu>
2024-04-19 17:12:14 -0400

    added previous button

--- src/components/progressBar.tsx
+++ src/components/progressBar.tsx
@@ -4,13 +4,16 @@ import { Form } from "react-bootstrap";
 export function ProgressBar(): JSX.Element{
     const [progress, setProgress] = useState<number>(0);
 
+    function updateProgress(){
+        setProgress(progress+1);
+    }
 
     return(
         <div>
-
+            <center>
             <progress value={progress} max={100} />
             <span>{progress}%</span>
-
+            </center>
         </div>
     );
 }
