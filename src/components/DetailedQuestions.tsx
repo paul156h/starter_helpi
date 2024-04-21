bd93f59b0290f98caf18093d1af6c19d285bd4eb
Brandon Cell <bcell@udel.edu>
2024-04-21 19:37:54 -0400

    progress bar working in detailed questions

--- src/components/DetailedQuestions.tsx
+++ src/components/DetailedQuestions.tsx
@@ -2,12 +2,20 @@ import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 
 
-export function DetailedQuestions({question,questionNumber,currentQuestion}: {question: string; questionNumber:number;currentQuestion:number;}): JSX.Element {
+export function DetailedQuestions({question,questionNumber,currentQuestion, updateNumAnswered}: {question: string; questionNumber:number;currentQuestion:number; updateNumAnswered: (value: number) => void}): JSX.Element {
     const [input, setInput] = useState<string>("");
+    const [alreadyAnswered, setAlreadyAnswered] = useState<boolean>(false);
     function updateInput(event: React.ChangeEvent<HTMLInputElement>){
         setInput(event.target.value);
+        if(alreadyAnswered && event.target.value === ""){
+            setAlreadyAnswered(false);
+            updateNumAnswered(-10)
+        }
+        else if(!alreadyAnswered){
+            setAlreadyAnswered(true);
+            updateNumAnswered(10);
+        }
     }
-
     return(
         <div>
             {questionNumber !== currentQuestion ? "" : <p>Question {questionNumber}: {question}</p>}
